72ed9623aed1b187c76a4edc761aa0e7f99ddee9
Josezarz <146075151+Josezarz@users.noreply.github.com>
2024-04-22 11:03:38 +0200

    DAO

--- src/main/java/application/model/CalendarioDAO.java
+++ src/main/java/application/model/CalendarioDAO.java
@@ -1,5 +1,36 @@
 package application.model;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.chrono.ChronoLocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
+import javafx.scene.control.DatePicker;
+
 public class CalendarioDAO {
 
+	/**
+	 * Función buscarFecha: la función buscará una fecha y abrirá una 
+	 * ventana emergente con dicha fecha. Utilizaremos un datePicker.
+	 * @return
+	 */
+	  public static int buscarFecha() {
+		  DatePicker fecha= new DatePicker();
+		  
+		  /*
+		   * Podemos establecer la fecha programáticamente usando el método setValue(LocalDate) 
+		   * del mismo modo obtenemos  la fecha actual a través de getValue() 
+		   * que devuelve un objeto LocalDate que puede ser nulo.
+		   */
+		  fecha.setValue(LocalDate.of(2000, Month.JANUARY, 1));    
+		  LocalDate date = fecha.getValue();
+		  
+		  //Le da un formato el cuale ne la fecha se ve ejemplo: Lunes 22 de abril de 2024
+		  DateTimeFormatter formatter = DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL);
+		  String formattedDate = ((ChronoLocalDate) fecha).format(formatter);
+
+	    
+		return -1;
+	  }
 }
--- src/main/java/application/model/OpcionesDAO.java
+++ src/main/java/application/model/OpcionesDAO.java
@@ -1,5 +1,31 @@
 package application.model;
 
+import java.sql.Connection;
+
 public class OpcionesDAO {
+
+	//Función activarNotificaciones: la función activará y desactivará las notificaciones.
+	public static int activarNotificaciones(int activo, Connection con) {
+		
+		return -1;
+	}
+	
+	//Función cambiarModo: la función cambiará el modo (claro u oscuro) de la aplicación.
+	public static int cambiarModo(int modo, Connection con){
+		
+		return -1;
+	}
+
+	//Función cambiarDiseño: la función cambiará el diseño de la aplicación.
+	public static int cambiarDiseño(int Modo, Connection con){
+		
+		return -1;
+	}
+	
+	//Función cambiarIdioma: la función cambiará el idioma (español o inglés) de la aplicación.
+	public static int cambiarIdioma(int idioma, Connection con){
+		
+		return -1;
+	}
 
 }
