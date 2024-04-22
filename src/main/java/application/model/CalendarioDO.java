bba918f8aa30182be3a72b784c3188a8e1df9f57
Josezarz <146075151+Josezarz@users.noreply.github.com>
2024-04-22 10:30:23 +0200

    DO

--- src/main/java/application/model/CalendarioDO.java
+++ src/main/java/application/model/CalendarioDO.java
@@ -1,5 +1,53 @@
 package application.model;
 
 public class CalendarioDO {
-
+	private int idCalendario;
+	private String Fecha;
+	private String Imagen_Calendario;
+	private int Usuario_idUsuario;
+	
+	public CalendarioDO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public CalendarioDO(int idCalendario, String fecha, String imagen_Calendario, int usuario_idUsuario) {
+		super();
+		this.idCalendario = idCalendario;
+		Fecha = fecha;
+		Imagen_Calendario = imagen_Calendario;
+		Usuario_idUsuario = usuario_idUsuario;
+	}
+	
+	public int getIdCalendario() {
+		return idCalendario;
+	}
+	public void setIdCalendario(int idCalendario) {
+		this.idCalendario = idCalendario;
+	}
+	public String getFecha() {
+		return Fecha;
+	}
+	public void setFecha(String fecha) {
+		Fecha = fecha;
+	}
+	public String getImagen_Calendario() {
+		return Imagen_Calendario;
+	}
+	public void setImagen_Calendario(String imagen_Calendario) {
+		Imagen_Calendario = imagen_Calendario;
+	}
+	public int getUsuario_idUsuario() {
+		return Usuario_idUsuario;
+	}
+	public void setUsuario_idUsuario(int usuario_idUsuario) {
+		Usuario_idUsuario = usuario_idUsuario;
+	}
+	
+	@Override
+	public String toString() {
+		return "CalendarioDO [idCalendario=" + idCalendario + ", Fecha=" + Fecha + ", Imagen_Calendario="
+				+ Imagen_Calendario + ", Usuario_idUsuario=" + Usuario_idUsuario + "]";
+	}
+	
 }
--- src/main/java/application/model/OpcionesDO.java
+++ src/main/java/application/model/OpcionesDO.java
@@ -1,5 +1,80 @@
 package application.model;
 
 public class OpcionesDO {
+	private int idOpciones;
+	private int Idioma;
+	private int Modo;
+	private int Notificaciones;
+	private int Fuente;
+	private int Diseño;
+	private int Ususario_idUsuario;
+	
+	public OpcionesDO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public OpcionesDO(int idOpciones, int idioma, int modo, int notificaciones, int fuente, int diseño,
+			int ususario_idUsuario) {
+		super();
+		this.idOpciones = idOpciones;
+		Idioma = idioma;
+		Modo = modo;
+		Notificaciones = notificaciones;
+		Fuente = fuente;
+		Diseño = diseño;
+		Ususario_idUsuario = ususario_idUsuario;
+	}
+	
+	public int getIdOpciones() {
+		return idOpciones;
+	}
+	public void setIdOpciones(int idOpciones) {
+		this.idOpciones = idOpciones;
+	}
+	public int getIdioma() {
+		return Idioma;
+	}
+	public void setIdioma(int idioma) {
+		Idioma = idioma;
+	}
+	public int getModo() {
+		return Modo;
+	}
+	public void setModo(int modo) {
+		Modo = modo;
+	}
+	public int getNotificaciones() {
+		return Notificaciones;
+	}
+	public void setNotificaciones(int notificaciones) {
+		Notificaciones = notificaciones;
+	}
+	public int getFuente() {
+		return Fuente;
+	}
+	public void setFuente(int fuente) {
+		Fuente = fuente;
+	}
+	public int getDiseño() {
+		return Diseño;
+	}
+	public void setDiseño(int diseño) {
+		Diseño = diseño;
+	}
+	public int getUsusario_idUsuario() {
+		return Ususario_idUsuario;
+	}
+	public void setUsusario_idUsuario(int ususario_idUsuario) {
+		Ususario_idUsuario = ususario_idUsuario;
+	}
+	
+	@Override
+	public String toString() {
+		return "OpcionesDO [idOpciones=" + idOpciones + ", Idioma=" + Idioma + ", Modo=" + Modo + ", Notificaciones="
+				+ Notificaciones + ", Fuente=" + Fuente + ", Diseño=" + Diseño + ", Ususario_idUsuario="
+				+ Ususario_idUsuario + "]";
+	}
 
+	
 }
