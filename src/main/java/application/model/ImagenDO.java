9eef7a051a67a04d2d34dda97772fa0b91cf9df7
Jeremy-buendia <jcerber343@g.educaand.es>
2024-04-23 10:12:13 +0200

    Añadido el Utils y cambios en ImagenDO

--- src/main/java/application/model/ImagenDO.java
+++ src/main/java/application/model/ImagenDO.java
@@ -1,5 +1,20 @@
 package application.model;
 
-public class ImagenDO {
+import java.sql.Connection;
 
+public class ImagenDO {
+	private int idImagen;
+	private String Nombre_imagen;
+	private String Ubicacion;
+	private String Fecha_Imagen;
+	private int Usuario_idUsuario;
+	private int Marcado;
+
+	public static int subirFoto(Connection con, int idUsuario) {
+		try {
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
 }
