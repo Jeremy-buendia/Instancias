58ac1f86e13c037fd0cae31391f01119d0b1ce26
JRamonVG <152074692+GithubAccount9999@users.noreply.github.com>
2024-04-25 18:23:27 +0200

    usuario

--- src/main/java/application/model/NotificacionesDO.java
+++ src/main/java/application/model/NotificacionesDO.java
@@ -2,4 +2,47 @@ package application.model;
 
 public class NotificacionesDO {
 
+	
+	private int idNotificaciones;
+	private int evento;
+	private String mensaje;
+	
+	
+	public int getIdNotificaciones() {
+		return idNotificaciones;
+	}
+	public void setIdNotificaciones(int idNotificaciones) {
+		this.idNotificaciones = idNotificaciones;
+	}
+	public int getEvento() {
+		return evento;
+	}
+	public void setEvento(int evento) {
+		this.evento = evento;
+	}
+	public String getMensaje() {
+		return mensaje;
+	}
+	public void setMensaje(String mensaje) {
+		this.mensaje = mensaje;
+	}
+	
+	public NotificacionesDO(int idNotificaciones, int evento, String mensaje) {
+		super();
+		this.idNotificaciones = idNotificaciones;
+		this.evento = evento;
+		this.mensaje = mensaje;
+	}
+	
+	public NotificacionesDO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public String toString() {
+		return "NotificacionesDO [idNotificaciones=" + idNotificaciones + ", evento=" + evento + ", mensaje=" + mensaje
+				+ "]";
+	}
+	
+	
 }
--- src/main/java/application/model/UsuarioDO.java
+++ src/main/java/application/model/UsuarioDO.java
@@ -2,4 +2,60 @@ package application.model;
 
 public class UsuarioDO {
 
+	private int id;
+	private String nombre;
+	private String apellido;
+	private String correo;
+	private String contraseña;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getCorreo() {
+		return correo;
+	}
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+	public String getContraseña() {
+		return contraseña;
+	}
+	public void setContraseña(String contraseña) {
+		this.contraseña = contraseña;
+	}
+	public UsuarioDO(int id, String nombre, String apellido, String correo, String contraseña) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.correo = correo;
+		this.contraseña = contraseña;
+	}
+	public UsuarioDO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public String toString() {
+		return "UsuarioDO [id=" + id + ", nombre=" + nombre + ", apellido=" + apellido + ", correo=" + correo
+				+ ", contraseña=" + contraseña + "]";
+	}
+	
+	
+	
 }
